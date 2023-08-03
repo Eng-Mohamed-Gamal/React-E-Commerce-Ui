1f91b2a8700c9cc85dd59053d610eb9f13013be6
Mohamed gamal <mohamed555asdfgh@gmail.com>
2023-08-04 01:14:52 +0300

    steps

--- src/Components/All/All.js
+++ src/Components/All/All.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from '../Home/Home'
+import Deals from '../Deals/Deals'
 
 export default function All() {
   return (
  <div className="all">
     <Home></Home>
+    <Deals></Deals>
  </div>
   )
 }
---
+++ src/Components/Deals/Deals.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Deals() {
+  return (
+<div className="deals">
+<h1 className='cen' >  <i class="fa-solid fa-bolt"></i>  Flash <span>Deals</span> </h1>
+</div>
+  )
+}
