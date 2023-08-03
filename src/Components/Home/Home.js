1ca4effc54880254e84209d67409531743af83a3
Mohamed gamal <mohamed555asdfgh@gmail.com>
2023-08-03 14:22:57 +0300

    Steps

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+<div className="home">
+  
+</div>
   )
 }
--- src/common/Header/Header.js
+++ src/common/Header/Header.js
@@ -9,6 +9,7 @@ export default function Header() {
         <h1>Bonik</h1>
         <ul>
             <li><NavLink to={"/"} > <i class="fa-solid fa-house"></i>   </NavLink></li>
+            <li><NavLink to={"/Cart"} > <i class="fa-solid fa-cart-shopping"></i> <span></span>  </NavLink></li>
         </ul>
 </div>
     </div>
